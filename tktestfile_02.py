ffbdceef15982183ed0bcf215972422f6c04ba86
Robert Tonkavich <rmtonkavich@gmail.com>
2020-01-04 10:46:41 -0500

    Create tktestfile_02.py

---
+++ tktestfile_02.py
@@ -0,0 +1,10 @@
+#Testing TKinter on Python3
+
+import tkinter
+
+tkinter._test()
+
+# This file works in a Bash Terminal
+# Ran with the following command
+# rober.....$ python3 tktestfile.py
+# opens a TKinter GUI Window.....
